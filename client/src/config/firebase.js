757689c0b168545c4b515145cdfdebf56ef9ba11
Sarasbari <sarasbari07@gmail.com>
2026-03-06 22:02:11 +0530

    improved firebase api security

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -4,14 +4,14 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAKFMWnW6MTKB2asdfs-6CazavmPXiKm40",
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "govtech-83e55.firebaseapp.com",
-    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://govtech-83e55-default-rtdb.firebaseio.com",
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "govtech-83e55",
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "govtech-83e55.firebasestorage.app",
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "892280634461",
-    appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:892280634461:web:45ae004ca951a79195a1b4",
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-77J68F2V5J"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
